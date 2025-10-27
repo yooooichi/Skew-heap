d2b52bdcb50dd1c40f24f3f9395435a19c3c8695
henry <mangoreal2004@gmail.com>
2025-10-27 18:08:02 -0500

    Implement merge function and update insertar prompts
    Added merge function to handle merging of two skew heaps and updated the insertar function to prompt for incident location.

--- variablesSkewHeap.h
+++ variablesSkewHeap.h
@@ -5,17 +5,32 @@ struct Nodo { // min heap sesgado
     int dato; // prioridad
     string ubicacion;
     string descripcion;
+    
     Nodo* izq;
     Nodo* der;
     
     Nodo(int v, string x, string z) : dato(v), ubicacion(x), descripcion(z), izq(nullptr), der(nullptr) {};
 };
 
+Nodo* merge(Nodo* h1, Nodo* h2) {
+    if (!h1) return h2;
+    if (!h2) return h1;
+
+    if (h1->dato > h2->dato)
+        swap(h1, h2);
+
+    h1->der = merge(h1->der, h2);
+
+    swap(h1->izq, h1->der);
+
+    return h1;
+}
+
 Nodo* insertar(Nodo* aux1) {
 	string s1, s2;
 	int b;
 	
-	cout << "Ingrese el nombre del incidente: ";
+	cout << "Ingrese la ubicacion del incidente: ";
 	getline(cin, s1);
 	
 	cout << "Ingrese una descripcion para el incidente: ";
@@ -28,4 +43,11 @@ Nodo* insertar(Nodo* aux1) {
     merge(aux1, temp); // donde aux1 es la raiz
     
     return aux1;
+}
+
+void fusionarDosZonas(Nodo* zona1, Nodo* zona2){
+    cout<<"Debido a la falta de recursos, se va a proceder a fusionar la zona de atención "<<zona1->ubicacion<<" y "<<zona2->ubicacion<<". "<<endl;
+    merge(zona1, zona2);
+    
+    cout<<"Zonas fusionadas correctamente."<<endl;
 }

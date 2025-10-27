1c37e7dd9d00516b0c94e454927fdc5bb8c5e9ff
yuichi <123523778+yooooichi@users.noreply.github.com>
2025-10-27 18:03:16 -0500

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,71 @@
+int main() {
+  int opc;
+    
+    do{
+        cout<<"Simulación de programa"<<endl;
+        cout<<"1.Insertar Incidentes"<<endl;
+        cout<<"2.Solucionar incidente"<<endl;
+        cout<<"3.Fusionar zonas"<<endl;
+        cout<<"4.Insertar zonas"<<endl;
+        cout<<"5.Salir"<<endl;
+        cout<<"Elija una opcion:";
+        cin >> opc;
+        cin.ignore();
+        
+        switch (opc){
+          case 1:{
+              if(zonas.empty()){
+                  cout<<"No existen zonas.";
+                  break;
+              }
+              mostrarZonas();
+            int a;
+            cin>>a;
+            insertar(zonas[a]);
+            break;
+              
+          }
+            
+          
+          case 2: {
+              //remove(HeapPrincipal);
+            break;
+              
+          }
+            
+          
+          case 3:{
+              cout <<"Zonas disponibles a mezclar:" <<endl;
+            mostrarZonas();
+            cout<<"Escoja dos zonas a mezclar: ";
+            int x, y;
+            cin >>x;
+            cin>>y;
+            fusionarDosZonas(zonas[x], zonas[y]);
+            break;
+              
+          }
+            
+          
+          case 4:{
+              InsertarZona();
+            break;
+          }
+            
+          
+          case 5:{
+              cout<<"Saliendo del programa...";
+            break;
+          }
+            
+          
+          default:
+            cout<<"Ingrese una opcion valida";
+            break;
+          
+          
+            
+        }
+    }while(opc!=5);
+    return 0;
+}

16308acaaa2dbce4fdec32215e4c2cc62e34c853
henry <mangoreal2004@gmail.com>
2025-10-27 23:43:09 -0500

    Improve user interaction and function call handling
    mismo main que leftist heap

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <vector>
+#include "SkewHeap.h"
+using namespace std;
+
 int main() {
   int opc;
     
@@ -8,68 +13,72 @@ int main() {
         cout<<"3.Fusionar zonas"<<endl;
         cout<<"4.Insertar zonas"<<endl;
         cout<<"5.Salir"<<endl;
-        cout<<"Elija una opcion:";
+        cout<<"Elija una opcion: ";
         cin >> opc;
-        cin.ignore();
         
         switch (opc){
           case 1:{
               if(zonas.empty()){
-                  cout<<"No existen zonas.";
+                  cout<<"Todavía no hay zonas registradas."<<endl;
                   break;
               }
               mostrarZonas();
             int a;
+            cout << "Escoja a qué zona desea insertar: ";
             cin>>a;
-            insertar(zonas[a]);
+            zonas[a] = insertar(zonas[a]);
             break;
               
           }
             
-          
           case 2: {
+              
+              if (zonas.empty()) {
+                cout << "No existen zonas registradas actualmente." << endl;
+                break;
+                }
+              
               mostrarZonas();
               cout<<"Escoja una zona:";
               int i;
               cin>>i;
-            atenderIncidente(zonas[i]);
+            zonas[i] = atenderIncidente(zonas[i]);
             break;
               
           }
             
-          
           case 3:{
-              cout <<"Zonas disponibles a mezclar:" <<endl;
+            if (zonas.size() < 2) {
+                    cout << "Se necesitan al menos dos zonas para fusionar." << endl;
+                    break;
+                }
+            cout <<"Zonas disponibles a mezclar:" <<endl;
             mostrarZonas();
-            cout<<"Escoja dos zonas a mezclar: ";
+            cout<<"Escoja dos zonas a mezclar: "<<endl;
             int x, y;
-            cin >>x;
-            cin>>y;
-            fusionarDosZonas(zonas[x], zonas[y]);
+            cout<<"Zona 1:";cin >>x;
+            cout<<"Zona 2:";cin>>y;
+            fusionarDosZonas(x, y);
             break;
               
           }
             
-          
           case 4:{
               InsertarZona();
             break;
           }
             
-          
           case 5:{
               cout<<"Saliendo del programa...";
             break;
           }
             
-          
           default:
             cout<<"Ingrese una opcion valida";
             break;
           
-          
-            
         }
+        cout<<endl;
     }while(opc!=5);
     return 0;
 }
